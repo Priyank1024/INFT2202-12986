41f591b3d8a11d1f29cd6b6da2fb9f4963daf180
Priyank1024 <169079131+Priyank1024@users.noreply.github.com>
2025-01-13 19:01:54 -0500

    first commit

---
+++ WEEK_1/SCRIPTS/APP.js
@@ -0,0 +1,9 @@
+//IIFE - Immediately Invoked Functional Expression
+(function () {
+
+    function Start(){
+        console.log("Starting...");
+    }
+    window.addEventListener("Load", Start);
+
+})();
